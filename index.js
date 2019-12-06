dd6286f6bb38ad732bf453db99caa4ca0d79cc9e
Colyn Prater <colyn.prater@moneysmart.com>
2019-12-06 14:46:30 +0800

    Update action yml and write script for parsing task_definition

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
+// TODO compile packages and this script to the big file
+// https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-a-javascript-action
+
 const core = require('@actions/core');
+const fs = require('fs');
+const path = require('path');
 
-// 1. read the template json file
-// 2. read the ENV
-//    ENV_VAR_ONE=1
-//    ENV_VAR_TWO=2
-//    ENV_VAR_THREE=3
-// 3. replace the `environment` attribute
-//    {
-//      "name": "ENV_VAR_ONE",
-//      "value": 1
-//    }
-// 4. output a new JSON response
+// Get the task_definition file contents
+let taskDefinitionFilePath = core.getInput('task_definition_template_path');
 
-// This is how you return a value
-// core.setOutput('task_definition', JSON.stringify(bla));
+const taskDefinitionFile = path.isAbsolute(taskDefinitionFilePath) ?
+  taskDefinitionFilePath : path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFilePath);
 
+if ( !fs.existsSync(taskDefinitionFile) ) {
+  throw new Error(`Task definition file does not exist: ${taskDefinitionFile}`);
+}
+const taskDefContents = require(taskDefinitionFile);
 
-// This is how this person detected where the file is
-//  const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
-//       taskDefinitionFile :
-//       path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
-//     if (!fs.existsSync(taskDefPath)) {
-//       throw new Error(`Task definition file does not exist: ${taskDefinitionFile}`);
-//     }
-//     const taskDefContents = require(taskDefPath);
-// https://github.com/aws-actions/amazon-ecs-render-task-definition/blob/master/index.js
+// Format env vars for task definition file
+let envVarStringArray = core.getInput('secret_keys').split("\n");
+envVarStringArray.splice(-1); // remove empty string element from the end
+
+const secretKeys = envVarStringArray.map( (envVar) => {
+  let splitEnvVar = envVar.split("=");
+
+  if ( ['true', 'false'].includes(splitEnvVar[1]) ) {
+    splitEnvVar[1] = splitEnvVar[1] == 'true';
+  }
+
+  return { "name": splitEnvVar[0], "value": splitEnvVar[1] };
+})
+
+// Replace 'environment' key in task_definition with parsed values from Vault
+taskDefContents.containerDefinitions[0].environment = secretKeys;
+
+// Output a new JSON response
+core.setOutput('task_definition', JSON.stringify(taskDefContents));
