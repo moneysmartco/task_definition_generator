e717660cbb72f60187471b4f2a91c036d3e8c036
Benjamin Robinson <benjaminrobinson1313@gmail.com>
2019-12-17 11:09:52 +0800

    New version

--- index.js
+++ index.js
@@ -18,34 +18,21 @@ try {
   // Format env vars for task definition file
   let envVarStringArray = core.getInput('secret_keys').split("\\n");
   core.debug(envVarStringArray)
-  console.log(envVarStringArray)
 
   envVarStringArray.splice(-1); // remove empty string element from the end
 
-  core.debug(envVarStringArray)
-  console.log(envVarStringArray)
-
   const secretKeys = envVarStringArray.map( (envVar) => {
     let splitEnvVar = envVar.split("=");
-
-    if ( ['true', 'false'].includes(splitEnvVar[1]) ) {
-      splitEnvVar[1] = splitEnvVar[1] == 'true';
-    }
-
     return { "name": splitEnvVar[0], "value": splitEnvVar[1] };
   });
 
   // Replace 'environment' key in task_definition with parsed values from Vault
-  core.debug(secretKeys)
-  console.log(secretKeys)
   taskDefContents.containerDefinitions[0].environment = secretKeys;
 
-  console.log(taskDefContents.containerDefinitions[0].environment)
   // replace 'name' key in task definition with name set in github action
   taskDefContents.containerDefinitions[0].name = containerName;
 
   // Output a new JSON response
-  core.debug(taskDefContents)
   console.log(taskDefContents)
   core.setOutput('task_definition', JSON.stringify(taskDefContents));
 } catch (error) {
